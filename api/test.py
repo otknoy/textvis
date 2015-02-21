d8cdd0cbe0695289f552dfa8edb564bb1c84adbb
otknoy <otknoy@gmail.com>
2015-02-22 01:52:50 +0900

    temp

--- api/test.py
+++ api/test.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
-print "Content-type: text/html"
-print
-print "<html>"
-print "<center>Hello, Linux.com!</center>"
-print "</html>"
+# coding: utf-8
+from bottle import route, request, run
+import json
+
+import nlp.preprocess
+
+
+@route('/tokenize', method='POST')
+def tokenize():
+    text = request.forms.get('text')
+    text = nlp.preprocess.normalize(text.decode('utf-8'))
+    terms = nlp.preprocess.tokenize(text)
+    return '|||'.join(terms).encode('utf-8')
+    # return json.dumps(terms)
+
+run(host='localhost', port=8888, debug=True, reloader=True)
